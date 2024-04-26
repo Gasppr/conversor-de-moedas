3bf7731fa9957c9def58de8a71655b00470190a2
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-04-25 23:26:00 -0300

    feat: Criação da classe Conversao.
    description: A classe Conversao é a classe principal da aplicação, onde tem os metódos "converter" onde abrirá uma interface no terminal para o usuário, Em seguida o metódo "conversor" que irá fazer as requisições e trazer os resultados em tipo Cambio para a nossa aplicação.

---
+++ conversorDeMoedas/src/model/Conversao.java
@@ -0,0 +1,108 @@
+package model;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class Conversao {
+
+ public void converter(){
+
+     Scanner sc = new Scanner(System.in);
+     Cambio cambio;
+
+     int opBase , opTarget;
+     int op = 1;
+
+     while (op != 0){
+
+        String base, target;
+        double valor;
+
+        System.out.println("Bem-vindo ao conversor Alura + Oracle");
+        System.out.println("***********************************************************");
+
+        System.out.println("Qual é a sua moeda?");
+        // pelo menos 6 tipos de moedas
+        System.out.println("""
+                1 - BRL (Moeda Brasileira)
+                2 - USD (Moeda Estadunidense)
+                3 - EUR (Moeda Europeia)
+                4 - CNY (Moeda Chinesa)
+                5 - RUB (Moeda Russa)
+                6 - ARS (Moeda Argentina)
+                7 - Outro
+                """);
+        opBase = sc.nextInt();
+        if (opBase == 1) base = "BRL";
+        else if (opBase == 2) base = "USD";
+        else if (opBase == 3) base = "EUR";
+        else if (opBase == 4) base = "CNY";
+        else if (opBase == 5) base = "RUB";
+        else if (opBase == 6) base = "ARS";
+        else {
+            System.out.println("Digite a abreviação da moeda que deseja");
+            base = sc.next().toUpperCase();
+        }
+
+        System.out.println("Para qual deseja converter?");
+        System.out.println("""
+                1 - BRL (moeda Brasileira)
+                2 - USD (moeda Estadunidense)
+                3 - EUR (moeda Europeia)
+                4 - CNY (Moeda Chinesa)
+                5 - RUB (Moeda Russa)
+                6 - ARS (Moeda Argentina)
+                7 - Outro
+                """);
+        opTarget = sc.nextInt();
+        if (opTarget == 1) target = "BRL";
+        else if (opTarget == 2) target = "USD";
+        else if (opTarget == 3) target = "USD";
+        else if (opTarget == 4) target = "CNY";
+        else if (opTarget == 5) target = "RUB";
+        else if (opTarget == 6) target = "ARS";
+        else {
+            System.out.println("Digite a abreviação da moeda que deseja");
+            target = sc.next().toUpperCase();
+        }
+
+        System.out.println("Qual é o valor?");
+        valor = sc.nextDouble();
+
+        cambio = conversor(base , target, valor);
+
+        System.out.println(cambio.toString());
+
+        System.out.println("**************************************************************");
+        System.out.println("Deseja continuar a converter mais um valor? \n 1- CONTINUAR  \n 0- SAIR");
+        op = sc.nextInt();
+
+    }
+
+     System.out.println("*******************************************************");
+     System.out.println("Até a próxima!");
+
+ }
+
+public Cambio conversor(String base , String target , double valor){
+
+    URI url = URI.create("https://v6.exchangerate-api.com/v6/9e5316d5b1d9cd8e275aeee0/pair/"+ base +"/"+target+"/" + valor);
+    HttpRequest request = HttpRequest.newBuilder().uri(url).build();
+
+    try {
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return new Gson().fromJson(response.body(), Cambio.class);
+
+    } catch (IOException | InterruptedException e) {
+        throw new RuntimeException(e);
+    }
+
+ }
+
+}
