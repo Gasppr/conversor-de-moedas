47168b7cfcd5ef85de4a9857853edf3c026e839b
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-04-25 23:22:22 -0300

    feat: Criação da main class para a execução da aplicação.
    description: Main class para executar os metódos das classes principais do projeto.

---
+++ conversorDeMoedas/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import model.Conversao;
+
+public class Main {
+    public static void main(String[] args){
+        Conversao conversor = new Conversao();
+        conversor.converter();
+
+    }
+
+
+}
